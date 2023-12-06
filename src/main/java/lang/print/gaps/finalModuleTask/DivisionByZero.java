00d122e308be42e2d176f613d8d2dbcf2a05149d
IsNotAvailableName <148902549+IsNotAvailableName@users.noreply.github.com>
2023-12-07 00:37:56 +0300

    Done

--- src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
@@ -1,8 +1,11 @@
 package lang.print.gaps.finalModuleTask;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class DivisionByZero {
     public static void main(String[] args) {
-        _ number = 10;
-        System.out.println(number/0);
+        double number = 10;
+        System.out.println(number / 0);
     }
 }
